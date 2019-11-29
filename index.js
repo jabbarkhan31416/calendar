4ae638e80eb0b318d642ef836bdaa18831229c61
jabbarkhan31416 <jabbarkhan31416@gmail.com>
2019-11-29 05:31:33 +0530

    x

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+
+
+
 window.addEventListener("DOMContentLoaded", e=>{
     const table = document.getElementsByTagName("table")[0]
-
-    
+    const render = time=>{
+        const date = time.getDate()
+        const day = time.getDay()
+        const month = time.getMonth()
+        const year = time.getYear()
+        const febDays = (
+            year%4 !== 0 ? 28 :
+            year%400 === 0 ? 29 :
+            year%100 === 0 ? 28 :
+            29
+        )
+        const totalDays = [
+            31, febDays, 31,
+            30, 31, 30,
+            31, 31, 30,
+            31, 30, 31
+        ][month]
+        
+    }
 })
