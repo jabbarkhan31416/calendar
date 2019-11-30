ac6e74d9aabecad35e5d28d39cf2979e9ced002b
jabbarkhan31416 <jabbarkhan31416@gmail.com>
2019-11-30 21:46:19 +0530

    0

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 
 window.addEventListener("DOMContentLoaded", e=>{
     const table = document.getElementsByTagName("table")[0]
+    const datesArray =[
+        31, null, 31,
+        30, 31, 30,
+        31, 31, 30,
+        31, 30, 31
+    ]
     const render = time=>{
         const date = time.getDate()
         const day = time.getDay()
@@ -14,14 +20,9 @@ window.addEventListener("DOMContentLoaded", e=>{
             year%100 === 0 ? 28 :
             29
         )
-        const datesArray =[
-            31, febDates, 31,
-            30, 31, 30,
-            31, 31, 30,
-            31, 30, 31
-        ]
-        const totalDates = datesArray[month]
-        const preTotalDates = datesArray[month===0 ? 11 : month-1]
-        
+        const datesArray0 = datesArray.map(x => x?x:febDates)
+        const totalDates = datesArray0[month]
+        const preTotalDates = datesArray0[month===0 ? 11 : month-1]
+        const firstDay = (day-date+36) % 7
     }
 })
