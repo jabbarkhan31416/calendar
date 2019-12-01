a8976030bf9a4abc33cd42f585df0d224702d863
jabbarkhan31416 <jabbarkhan31416@gmail.com>
2019-12-01 16:44:18 +0530

    p

--- index.js
+++ index.js
@@ -25,5 +25,28 @@ window.addEventListener("DOMContentLoaded", e=>{
         const preTotalDates = datesArray0[month===0 ? 11 : month-1]
         const firstDay = (day-date+36) % 7
         const totalWeeks = Math.ceil((firstDay+totalDates) / 7)
+        {
+            while(table.childElementCount>3) table.removeChild(table.lastChild)
+            let i = 0
+            let j = 0
+            let k0 = preTotalDates-firstDay
+            let k1 = 0
+            let k2 = 0
+            while(i<totalWeeks){
+                i = i+1
+                let l = 0
+                const tr = document.createElement("tr")
+                tr.setAttribute("class", "tr-w"+totalWeeks)
+                table.appendChild(tr)
+                while(l<7 && k0<preTotalDates){
+                    l = l+1
+                    j = j+1
+                    k0 = k0+1
+                    const td = document.createElement("td")
+                    td.setAttribute("class", j%2 ? "odd" : "even")
+                }
+            }
+        }
     }
+    render(new Date())
 })
