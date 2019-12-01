d3a544723b675cbb03399c6561747275e28d6bb8
jabbarkhan31416 <jabbarkhan31416@gmail.com>
2019-12-02 00:33:34 +0530

    h

--- index.js
+++ index.js
@@ -2,24 +2,26 @@
 
 
 window.addEventListener("DOMContentLoaded", e=>{
-    const table = document.getElementsByTagName("table")[0]
+    const table = document.getElementsByTagName("tbody")[0]
     const datesArray =[
         31, null, 31,
         30, 31, 30,
         31, 31, 30,
         31, 30, 31
     ]
+    const isLeapYear = y=>(
+        year%4 !== 0 ? false :
+        year%400 === 0 ? true :
+        year%100 === 0 ? false :
+        true
+    )
     const render = time=>{
         const date = time.getDate()
         const day = time.getDay()
         const month = time.getMonth()
         const year = time.getYear()
-        const febDates = (
-            year%4 !== 0 ? 28 :
-            year%400 === 0 ? 29 :
-            year%100 === 0 ? 28 :
-            29
-        )
+
+        const febDates = isLeapYear(year)?29:28
         const datesArray0 = datesArray.map(x => x?x:febDates)
         const totalDates = datesArray0[month]
         const preTotalDates = datesArray0[month===0 ? 11 : month-1]
@@ -28,23 +30,26 @@ window.addEventListener("DOMContentLoaded", e=>{
         {
             while(table.childElementCount>3) table.removeChild(table.lastChild)
             let i = 0
-            let j = 0
-            let k0 = preTotalDates-firstDay
-            let k1 = 0
-            let k2 = 0
+            let j = (new Array(firstDay)).fill(0).map(
+                (_,x) => x+1+preTotalDates-firstDay
+            ).concat(
+                (new Array(totalDates).fill(0).map((_,x) => x+1))
+            ).concat(
+                (new Array(totalWeeks*7 - totalDates - firstDay)).map((_,x) => x+1)
+            )
+            console.log(j)
             while(i<totalWeeks){
-                i = i+1
-                let l = 0
+                let k = 0
                 const tr = document.createElement("tr")
                 tr.setAttribute("class", "tr-w"+totalWeeks)
                 table.appendChild(tr)
-                while(l<7 && k0<preTotalDates){
-                    l = l+1
-                    j = j+1
-                    k0 = k0+1
+                while(k<7){
                     const td = document.createElement("td")
-                    td.setAttribute("class", j%2 ? "odd" : "even")
+                    td.setAttribute("class", cssClass + ((i^k)%2 ? " odd" : " even"))
+                    tr.appendChild(td)
+                    k = k+1
                 }
+                i = i+1
             }
         }
     }
