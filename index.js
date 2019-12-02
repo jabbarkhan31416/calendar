ef914ee01f3d530178426d2d3f9d190b13d7fcb1
jabbarkhan31416 <jabbarkhan31416@gmail.com>
2019-12-02 22:51:19 +0530

    g

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 window.addEventListener("DOMContentLoaded", e=>{
     const table = document.getElementsByTagName("tbody")[0]
     let tdArray = []
+    let patternArray = []
     const datesArray =[
         31, null, 31,
         30, 31, 30,
@@ -20,10 +21,13 @@ window.addEventListener("DOMContentLoaded", e=>{
         const date = time.getDate()
         const day = time.getDay()
         const month = time.getMonth()
-        const year = time.getYear()
+        const year = time.getFullYear()
 
-        const febDates = isLeapYear(year)?29:28
-        const datesArray0 = datesArray.map(x => x?x:febDates)
+        const datesArray0 = datesArray.map(x=>(
+            x ? x :
+            isLeapYear(year) ? 29 :
+            28
+        ))
         const totalDates = datesArray0[month]
         const preTotalDates = datesArray0[month===0 ? 11 : month-1]
         const firstDay = (day-date+36) % 7
@@ -32,7 +36,7 @@ window.addEventListener("DOMContentLoaded", e=>{
         while(table.childElementCount>3) table.removeChild(table.lastChild)
         tdArray = []
         
-        document.getElementById("year").textContent = 1900+year
+        document.getElementById("year").textContent = year
         
         document.getElementById("month").textContent = [
             "January", "February", "March",
@@ -41,13 +45,31 @@ window.addEventListener("DOMContentLoaded", e=>{
             "October", "November", "December"
         ][month]
         
-        let arr = (new Array(totalWeeks)).fill(0).map((_,x)=>(
+        patternArray = (new Array(totalWeeks)).fill(0).map((_,x)=>(
             (new Array(7)).fill(0).map((_,y)=>(
                 7*x + y
+            )).map(e=>(
+                e<firstDay ? [1+e+preTotalDates-firstDay, -1] :
+                e < firstDay+totalDates ? [1+e-firstDay, 0] :
+                [1+e-firstDay-totalDates, 1]
             ))
         ))
 
-        console.log(arr)
+        tdArray = patternArray.map((a,i)=>{
+            const tr = document.createElement("tr")
+            tr.setAttribute("class", "tr-w"+totalWeeks)
+            table.appendChild(tr)
+            return a.map(([x], j)=>{
+                const td = document.createElement("td")
+                const id = "date-"+i+","+j
+                td.setAttribute("id", id)
+                td.textContent = x
+                tr.appendChild(td)
+                return td
+            })
+        })
+
+        console.log(patternArray)
     }
     render(new Date())
     console.log(tdArray)
